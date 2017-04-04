220ec06ab6a5c489f1e8a7c623ad1b9e0bfeb4d6
Vladimir <vladimir.yaryhin@gmail.com>
2017-04-04 12:44:47 +0300

    deleted view enc

--- app/components/app.component.ts
+++ app/components/app.component.ts
@@ -2,7 +2,6 @@ import { Component, ViewEncapsulation} from '@angular/core';
 
 @Component({
     selector: 'app',
-    encapsulation: ViewEncapsulation.None,
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.less']
 })
